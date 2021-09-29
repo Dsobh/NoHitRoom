b6b03956adefa48f075516b0107efe6f3e3f1baf
Dsobh <davidsobrin@gmail.com>
2021-09-29 18:19:20 +0200

    Movimiento modificado. Usamos translate en vez de velocity y aplicamos lerp para suavizar

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    const string HORIZONTAL = "Horizontal";
+    const string VERTICAL = "Vertical";
+
     [SerializeField, Tooltip("Velocidad a la que se mueve el jugador")]
     private float speed = 6.5f;
     private Vector2 moveDirection = Vector2.zero;
     private Rigidbody2D playerRB;
+
+    [SerializeField, Tooltip("Suavizado en el movimiento")]
+    private float smoothTime = 0.04f;
+
+    private Vector2 lastMovement = Vector2.zero;
+
     
     // Start is called before the first frame update
     void Start()
@@ -17,20 +26,17 @@ public class Movement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
-        
+    {
+        //Capturamos el input. GetAxisRaw devuelve valores 0 y 1, no intermedios
+        moveDirection.x = Input.GetAxisRaw(HORIZONTAL);
+        moveDirection.y = Input.GetAxisRaw(VERTICAL);
     }
 
-    void FixedUpdate() {
-        moveDirection.x = Input.GetAxis("Horizontal");
-        moveDirection.y = Input.GetAxis("Vertical");
-        
-        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
-        {
-            playerRB.velocity = Vector2.zero;
-            playerRB.angularVelocity = 0;
-        }else{
-            playerRB.velocity = moveDirection * speed;
-        }
+    void FixedUpdate()
+    {
+        //Calculamos el último movimiento a través de un Lerp para hacer el suavizado
+        lastMovement = Vector2.Lerp(lastMovement, moveDirection, smoothTime);
+        this.transform.Translate(lastMovement * speed);
+
     }
 }
